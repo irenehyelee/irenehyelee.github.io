1682dcebe4ac6ca2730b39f6a08b05be9f965173
irenehyelee <82015771+irenehyelee@users.noreply.github.com>
2021-04-05 22:59:53 -0700

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-# irenehyelee.github.io
---
+++ precache-manifest.49a192aa05c58ea0a994030862a506b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4649c22605540c87c6af",
+    "url": "/static/css/main.e96b096b.chunk.css"
+  },
+  {
+    "revision": "4649c22605540c87c6af",
+    "url": "/static/js/main.4649c226.chunk.js"
+  },
+  {
+    "revision": "91054b961b4bb7c5b794",
+    "url": "/static/js/1.91054b96.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d699565c1fac87df5ed13ec0cd47780a",
+    "url": "/index.html"
+  }
+];
